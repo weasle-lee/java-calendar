06a3f8b1f08aa2416d499fe94c077aa31f446422
이재혁 <@DESKTOP-98664C2>
2020-08-21 19:14:09 +0900

    입력받은 값 최대일수 구하기

--- src/calendar/calendar.java
+++ src/calendar/calendar.java
@@ -1,14 +1,25 @@
 package calendar;
 
-public class calendar {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-	
-		System.out.println(" 일   월   화   수   목   금   토");
-		System.out.println("-------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+
+public class calendar {
+	private static final int[] Max_Days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+	public int maxDaysofMonth(int month) {
+		return Max_Days[month -1];
 	}
+	
+	public static void main(String[] args) {
+	// 입력받은 값에 최대 일 수 구하기
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하시오 : ");
+		calendar cal = new calendar();
+		
+		int month = scanner.nextInt();
+		
+		System.out.println(month+"월은"+cal.maxDaysofMonth(month)+" 일 까지 있습니다.");
+		
+		
+	}
+
 }
